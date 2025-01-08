edfb4c44b345ce90434eae8dfa379ff932a0b976
manhhoach <manhhn.nv5@gmail.com>
2025-01-08 22:47:03 +0700

    .

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,11 +4,11 @@ export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
   useFactory: async (): Promise<TypeOrmModuleOptions> => {
     return {
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 3306,
+      username: process.env.DB_USERNAME || 'root',
+      password: process.env.DB_PASSWORD || 'manh123456',
+      database: process.env.DB_DATABASE || 'mysql',
       entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
       synchronize: true,
       logging: true,
